c4e948509d16383bd2c9352dcb117eb724fd1852
wabdsgn <wab2com@gmail.com>
2020-05-26 00:09:22 +0200

    💄 avatar & favicon update

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,11 +1,6 @@
 const Header = () => (
   <header className="flex flex-col items-center justify-center text-center min-h-64 text-lg">
-    <img
-      src="/avataaars.png"
-      alt="avatar"
-      width="120"
-      className="mx-auto p-4 m-4"
-    />
+    <img src="/avataaar.svg" alt="avatar" width="120" className="mx-auto m-4" />
     <p>jérôme boileux • frontend engineer</p>
     <blockquote className="text-gray-600 font-thin font-sans tracking-wider">
       Responding to complexity with simplicity
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -10,26 +10,11 @@ const Layout = ({ children }) => (
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
       {/* facicons */}
-      <link rel="icon" href="/favicon.ico" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon alternate"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon alternate"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="alternate icon" href="/favicon.ico" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff8a01" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <link rel="manifest" href="/site.webmanifest" />
       <meta name="apple-mobile-web-app-title" content="jérôme boileux" />
       <meta name="application-name" content="jérôme boileux" />
       <meta name="theme-color" content="#ffffff" />
