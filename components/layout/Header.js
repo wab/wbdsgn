6f6e56a53b7a297803e1f60618da9c982ea09ee8
wabdsgn <wab2com@gmail.com>
2020-05-30 15:51:07 +0200

    💫 add avatar animation

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,6 +1,8 @@
+import Avatar from './Avatar';
+
 const Header = () => (
   <header className="flex flex-col items-center justify-center text-center min-h-64 text-lg">
-    <img src="/avataaar.svg" alt="avatar" width="120" className="mx-auto m-4" />
+    <Avatar className="mx-auto m-4" />
     <p>jérôme boileux • frontend engineer</p>
     <blockquote className="text-gray-600 font-thin font-sans tracking-wider">
       Responding to complexity with simplicity
